fbebc70fab66190be3219a0e5dc977c3220a6bd2
giuliodondi <giuliodondi94@gmail.com>
2020-10-19 15:07:06 +0200

    updated exercises

--- exercises/c++/01_intro/convl.cc
+++ exercises/c++/01_intro/convl.cc
@@ -2,7 +2,7 @@
 #include <string>
 #include <cstdbool>
 
-double converter () {
+std::string converter () {
 	double input;
 	double factor{0};
 	std::string unit {""};
@@ -10,24 +10,42 @@ double converter () {
 	bool valid{false};
 	
 	while (!(valid)) {
-		std::cout << "Enter a value and its unit of measure, separated by spaces." << std:endl;
+		std::cout << "Enter a value and its unit of measure, separated by spaces." << std::endl;
 		
-		std::cin >> input >> unit
+		std::cin >> input >> unit;
+		
 			
 		if (!std::cin) {
-			std::cout << "Invalid input. Please retry." << std:endl;
+			std::cout << "\n" <<   "Invalid input. Please retry." << std::endl;
 		} else {
+			
+			//handle unit formatting
+			unsigned int unit_id{0};
+			if ( (unit.compare("m") == 0) || (unit.compare("metres")==0) || (unit.compare("Metres")==0) || (unit.compare("meters")==0) || (unit.compare("Meters")==0) ) {
+				unit_id=1;	
+			}
+			else if ( (unit.compare("ft") == 0) || (unit.compare("feet")==0) || (unit.compare("Feet")==0)  ) {
+				unit_id=2;	
+			}
+			else if ( (unit.compare("in") == 0) || (unit.compare("inches")==0) || (unit.compare("Inches")==0) ) {
+				unit_id=3;	
+			}
 		
-			switch(unit) {
-			  case m:
-				factor = 1;
-				valid=true;
-				break;
-			  case f:
-
-				break;
-			  default:
-				std::cout << "Unrecognised unit. Please retry." << std:endl;
+			switch(unit_id) {
+				case 1:
+					factor = 1;
+					valid=true;
+					break;
+				case 2:
+					factor = 0.3048;
+					valid=true;
+					break;
+				case 3:
+					factor = 0.0254;
+					valid=true;
+					break;
+				default:
+					std::cout << "\n" <<  "Unrecognised unit. Please retry." << std::endl;
 			}
 			
 		}
@@ -39,6 +57,8 @@ double converter () {
 		
 	}
 	
+	std::string output = std::to_string(input*factor) + " m";
+	return output;
 	
 }
 
@@ -46,9 +66,10 @@ int main () {
 
 	double len{0};
 	
-	std::cout << "Welcome to Length Unit Converter." << std:endl;
+	std::cout << "Welcome to Length Unit Converter." << std::endl;
+
 	
-	len= converter()
+	std::string result = converter();
 	
-	std::cout << "Result: " << len >> " m" << std:endl;
+	std::cout << "Result: " << result<< std::endl;
 }
